38a96badcf3a49024a6dff64494a7973f318efd6
rahulbhosale2563@gmail.com <samikshabhosale16@email.com>
2022-11-22 16:48:04 +0530

    Added: Refactor code to the generics

--- src/com/bridgelabz/FindMaximumNumbers.java
+++ src/com/bridgelabz/FindMaximumNumbers.java
@@ -1,49 +1,29 @@
 package com.bridgelabz;
 
-public class FindMaximumNumbers {
+public class FindMaximumNumbers<T extends Comparable<T>> {
 
-	public Integer getMaxWithInteger(Integer n1, Integer n2, Integer n3) {
-		Integer max = n1;
-		if (n2.compareTo(max) > 0) {
+	void getMax(T n1, T n2, T n3) {
+		T max = n1;
+		if (n2.compareTo(max) > 0)
 			max = n2;
-		} else if (n3.compareTo(max) > 0) {
+		if (n3.compareTo(max) > 0)
 			max = n3;
-		}
-		System.out.println("The maximum value between the three Integer values is : " + max);
-		return max;
-	}
-
-	public Float getMaxWithFloat(Float n1, Float n2, Float n3) {
-		Float max = n1;
-		if (n2.compareTo(max) > 0) {
-			max = n2;
-		} else if (n3.compareTo(max) > 0) {
-			max = n3;
-		}
-		System.out.println("The maximum value between the three Float values is : " + max);
-		return max;
-	}
-
-	public String getMaxWithString(String n1, String n2, String n3) {
-		String max = n1;
-		if (n2.compareTo(max) > 0) {
-			max = n2;
-		} else if (n3.compareTo(max) > 0) {
-			max = n3;
-		}
-		System.out.println("The maximum value between the three Float values is : " + max);
-		return " ";
+		System.out.println(max);
 	}
 
 	public static void main(String[] args) {
+
 		FindMaximumNumbers max = new FindMaximumNumbers();
 
 		System.out.println("Welcome To Find Maximun Problem Using Generics \n");
 
-		max.getMaxWithInteger(23, 42, 10);
+		System.out.println("The maximum value between the three integer - ");
+		max.getMax(23, 10, 42);
 
-		max.getMaxWithFloat(10.12f, 11.34f, 9.8f);
+		System.out.println("\nThe maximum value between the three Float - ");
+		max.getMax(10.12f, 11.34f, 9.8f);
 
-		max.getMaxWithString("Banana", "Apple", "Peach");
+		System.out.println("\nThe maximum value between the three String - ");
+		max.getMax("Banana", "Apple", "Peach");
 	}
 }
