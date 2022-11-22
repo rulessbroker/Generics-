5a9c232be8d0df5c71a20ebe90abab0e1616a377
rahulbhosale2563@gmail.com <samikshabhosale16@email.com>
2022-11-22 20:09:25 +0530

    Added: Refactoring the code

--- src/com/bridgelabz/FindMaximumNumbers.java
+++ src/com/bridgelabz/FindMaximumNumbers.java
@@ -1,8 +1,15 @@
 package com.bridgelabz;
 
 public class FindMaximumNumbers<T extends Comparable<T>> {
+	private T n1;
+	private T n2;
+	private T n3;
 
-	void getMax(T n1, T n2, T n3) {
+	public FindMaximumNumbers(T n1, T n2, T n3) {
+		this.n1 = n1;
+		this.n2 = n2;
+		this.n3 = n3;
+
 		T max = n1;
 		if (n2.compareTo(max) > 0)
 			max = n2;
@@ -13,17 +20,15 @@ public class FindMaximumNumbers<T extends Comparable<T>> {
 
 	public static void main(String[] args) {
 
-		FindMaximumNumbers max = new FindMaximumNumbers();
-
 		System.out.println("Welcome To Find Maximun Problem Using Generics \n");
 
 		System.out.println("The maximum value between the three integer - ");
-		max.getMax(23, 10, 42);
+		FindMaximumNumbers integerMax = new FindMaximumNumbers(23, 10, 42);
 
 		System.out.println("\nThe maximum value between the three Float - ");
-		max.getMax(10.12f, 11.34f, 9.8f);
+		FindMaximumNumbers floatMax = new FindMaximumNumbers(10.12f, 11.34f, 9.8f);
 
 		System.out.println("\nThe maximum value between the three String - ");
-		max.getMax("Banana", "Apple", "Peach");
+		FindMaximumNumbers stringMax = new FindMaximumNumbers("Banana", "Apple", "Peach");
 	}
 }
