54dac7dad1439619c9b00bdbcf9aa9e76430ff4b
rahulbhosale2563@gmail.com <samikshabhosale16@email.com>
2022-11-22 16:21:47 +0530

    Added: max value between three float values

--- src/com/bridgelabz/FindMaximumNumbers.java
+++ src/com/bridgelabz/FindMaximumNumbers.java
@@ -1,21 +1,34 @@
 package com.bridgelabz;
 
 public class FindMaximumNumbers {
-	
-	void getMax(Integer n1, Integer n2, Integer n3) {
+
+	void getMaxWithInteger(Integer n1, Integer n2, Integer n3) {
 		Integer max = n1;
 		if (n2.compareTo(max) > 0) {
 			max = n2;
 		} else if (n3.compareTo(max) > 0) {
 			max = n3;
 		}
-		System.out.println("The maximum value between the three integer is : "+max);
+		System.out.println("The maximum value between the three integer is : " + max);
 	}
+
+	void getMaxWithFloat(Float n1, Float n2, Float n3) {
+		Float max = n1;
+		if (n2.compareTo(max) > 0) {
+			max = n2;
+		} else if (n3.compareTo(max) > 0) {
+			max = n3;
+		}
+		System.out.println("The maximum value between the three Float values is : " + max);
+	}
+
 	public static void main(String[] args) {
 		FindMaximumNumbers max = new FindMaximumNumbers();
-		
+
 		System.out.println("Welcome To Find Maximun Problem Using Generics \n");
-		
-		max.getMax(23,42,10);
+
+		max.getMaxWithInteger(23, 42, 10);
+
+		max.getMaxWithFloat(10.12f, 11.34f, 9.8f);
 	}
 }
